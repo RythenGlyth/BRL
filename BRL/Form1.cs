1dcb8b92535999974978b7569d979600945a21d2
RythenGlyth <rythenglyth@gmail.com>
2019-06-09 08:10:34 +0200

    AutoUpdate

--- BRL/Form1.cs
+++ BRL/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading;
 
 namespace BRL
 {
@@ -22,6 +26,46 @@ namespace BRL
             PanelSlide.Width = 0;
             panelSlideHidden = true;
             itemShop1.BringToFront();
+
+            update();
+        }
+
+        async public void update()
+        {
+            var currentVersion = Assembly.GetEntryAssembly().GetName().Version;
+            string jsonReponse = await getNewestUpdateResponse();
+
+            dynamic json = JsonConvert.DeserializeObject(jsonReponse);
+
+            var newVersion = new Version(Convert.ToString(json.tag_name));
+            if(newVersion > currentVersion)
+            {
+                DialogResult dialogResult = MessageBox.Show("Update available, \nDo you want to update? \n\nYou have version " + currentVersion.ToString() + " and newest version is " + newVersion.ToString(), "Update available", MessageBoxButtons.YesNo);
+                if(dialogResult == DialogResult.Yes)
+                {
+                    ShowUpdateForm(Convert.ToString(json.assets[0].browser_download_url));
+
+
+                }
+            }
+
+        }
+
+        public static void ShowUpdateForm(string url)
+        {
+            var updateForm = new DownloadUpdateDialog(url);
+            if (updateForm.ShowDialog().Equals(DialogResult.OK))
+            {
+                Application.Exit();
+            }
+        }
+
+        async public Task<string> getNewestUpdateResponse()
+        {
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("C# App");
+            return await httpClient.GetStringAsync("https://api.github.com/repos/RythenGlyth/BRL/releases/latest");
         }
 
         Point lastPoint;
